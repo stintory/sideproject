a584237ed6944262910f918fa0bdc8564fba5473
dhkang <dhkang@letsee.io>
2024-03-19 12:30:00 +0900

    [dev] add AuthModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './service/users.service';
+import { UsersRepository } from './repository/users.repository';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
