d49ae6b5303e7103a3f905cc7122ea85b2646d44
stintory <kdhcrusade@gmail.com>
2024-09-12 11:23:17 +0900

    [dev] 친구관게 요청 닉네임 추가

--- src/relationrequest/service/relationrequest.service.ts
+++ src/relationrequest/service/relationrequest.service.ts
@@ -15,10 +15,30 @@ export class RelationrequestService {
     const userId = new Types.ObjectId(user._id);
     const requesterId = userId;
 
-    const result = await this.relationsRepository.find({ requesterId });
-    if (result.length == 0) {
+    const requests = await this.relationsRepository.find({ requesterId, status: 'pending' });
+    if (requests.length == 0) {
       return { message: 'none' };
     }
+
+    const recipientIds = requests.map((request) => request.recipientId);
+    console.log(recipientIds);
+
+    const recipients = await this.usersRepository.find({ _id: { $in: recipientIds } });
+
+    const recipientNicknames = recipients.reduce((map, user) => {
+      map[user._id.toString()] = user.nickname; // Adjust the property name if different
+      return map;
+    }, {});
+
+    const result = requests.map((request) => ({
+      _id: request._id,
+      requesterId: request.requesterId,
+      recipientId: request.recipientId,
+      receivedUser: recipientNicknames[request.recipientId.toString()],
+      type: request.type,
+      status: request.status,
+    }));
+
     return { result };
   }
 
@@ -26,10 +46,29 @@ export class RelationrequestService {
     const userId = new Types.ObjectId(user._id);
     const recipientId = userId;
 
-    const result = await this.relationsRepository.find({ recipientId });
-    if (result.length == 0) {
+    const requests = await this.relationsRepository.find({ recipientId, status: 'pending' });
+    if (requests.length == 0) {
       return { message: 'none' };
     }
+
+    const requesterIds = requests.map((request) => request.requesterId);
+
+    const requesters = await this.usersRepository.find({ _id: { $in: requesterIds } });
+
+    const requesterNicknames = requesters.reduce((map, user) => {
+      map[user._id.toString()] = user.nickname;
+      return map;
+    }, {});
+
+    const result = requests.map((request) => ({
+      _id: request._id,
+      requesterId: request.requesterId,
+      requesterUser: requesterNicknames[request.requesterId.toString()],
+      recipientId: request.recipientId,
+      type: request.type,
+      status: request.status,
+    }));
+
     return { result };
   }
 
